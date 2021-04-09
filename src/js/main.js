c39f867f9303ddb4df3d5009083c39603e6e4070
Krzem <krzem5.dev@gmail.com>
2021-04-09 18:44:26 +0200

    Push Update 04/09/2021, 18:44:13

--- src/js/main.js
+++ src/js/main.js
@@ -32,7 +32,7 @@ function mouseMove(e){
 }
 function create_lines(){
 	fetch("./data/board.txt").then((r)=>r.text()).then(function(b){
-		let b=b.split("\n")
+		b=b.split("\n")
 		let w=b[0].split(" ")[0]
 		let h=b[0].split(" ")[1]
 		let CW=50
